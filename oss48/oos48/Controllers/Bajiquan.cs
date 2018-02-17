b0f20795a1c9c24fe68aa123061575c02bd39487
Luca Mori <mori.luca@hotmail.it>
2018-02-17 16:01:06 +0100

    Ricreato nuovo progetto net core mvc

---
+++ oss48/oos48/Controllers/Bajiquan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using L_Email;
+
+namespace oos48.Controllers
+{
+    public class BajiquanController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Corso()
+        {
+            ViewBag.Message = "Corso";
+
+            return View();
+        }
+
+        public ActionResult Istruttore()
+        {
+            ViewBag.Message = "Istruttore";
+            //L_Email.EmailSender email = new EmailSender(EmailSender.eEmailAccount.LucaGiDiAutomazione);
+            //email.SendEmail("lucam@gidiautomazione.it","oss48","Ecco l'email inviata dal sito",false);
+            //email = null;
+            return View();
+        }
+
+        public ActionResult Orari()
+        {
+            ViewBag.Message = "Orari";
+            return View();
+        }
+    }
+}

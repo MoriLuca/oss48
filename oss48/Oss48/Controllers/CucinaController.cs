a64d778f36094ea334ca78f2f87d5a547393268f
Luca Mori <mori.luca@hotmail.it>
2018-02-17 21:01:08 +0100

    creazione controller e view tipoprodotto

---
+++ oss48/Oss48/Controllers/CucinaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Oss48.Controllers
+{
+    public class CucinaController : Controller
+    {
+        // GET: Cucina
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ingredienti()
+        {
+            List<DB.Cucina.tipoprodotto> i = new List<DB.Cucina.tipoprodotto>();
+            using (var context = new DB.Cucina.cucinaEntities())
+            {
+                i = context.tipoprodotto.ToList();
+            }
+            return View(i[0]);
+        }
+
+    }
+}
